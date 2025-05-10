959340d294027fb99f9b2da4c2a888aec8676081
ericsalvi67 <ericsalvi67@gmail.com>
2025-05-10 10:45:44 -0300

    🐛   Update AboutViewModel to allow nullable properties for Nome and Description

--- webdev/WebASPNET/Models/AboutViewModel.cs
+++ webdev/WebASPNET/Models/AboutViewModel.cs
@@ -3,12 +3,9 @@
    {
       public class AboutViewModel
       {
-         public AboutViewModel(){
-         }
-
-         public string Nome { get; set; }
+         public string? Nome { get; set; }
          public int Idade { get; set; }
          public DateTime DataNascimento { get; set; }
-			public string Description { get; set; }
+			public string? Description { get; set; }
       }
    }
